be09a9e1e35fe2e34d0656aef5a40a9e86c182f8
WilliamRHarper <Wrharpe1230@gmail.com>
2020-06-13 15:23:31 -0500

    changes to logic added multiple users and multiple users onload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-// window.onload = function() {
-//     getPerson()
-// }
+window.onload = function() {
+    getMultiple();
+}
 let arrayOfPersons = [];
 const getPerson = () => {
     fetch('https://randomuser.me/api/')
@@ -24,7 +24,7 @@ const getPerson = () => {
         list.appendChild(li)
         list.appendChild(button)
         button.appendChild(buttonInfo)
-        // liInfo.appendChild(textInfo)
+        liInfo.appendChild(textInfo)
         button.addEventListener("click", function(){
             // const newList = document.createElement('ul')
             // const liInfo = document.createElement('li')
@@ -38,18 +38,40 @@ const getPerson = () => {
 
 
 
-// const getMultiple = () => {
-//     fetch('https://randomuser.me/api/results=5')
-//     .then(res => res.json())
-//     .then(person => arrayOfPersons.push(person))
-//     .catch((error) => {
-//         console.error('Error', error);
-//     });
-//     arrayOfAddresses.forEach(person => {
-//         person.result.name.first
-//         console.log(arrayOfPersons)
-//     })
-// }
+const getMultiple = () => {
+    fetch('https://randomuser.me/api/?results=5')
+    .then(res => res.json())
+    .then(user => user.results.map(person => {
+        arrayOfPersons.push(person)
+        console.log(person)
+        const list = document.getElementById("allUsers")
+        const pic = document.createElement("img")
+        pic.setAttribute('src', `${person.picture.thumbnail}`)
+        const li = document.createElement("li")
+        const text = document.createTextNode(`${person.name.first}`)
+        const button = document.createElement('button')
+        const buttonInfo = document.createTextNode('More Info')
+        const liInfo = document.createElement('li')
+        const textInfo = document.createTextNode(`${person.phone}, ${person.email}, ${person.dob}`)
+        const lineBreak = document.createElement('br')
+        li.appendChild(pic)
+        li.appendChild(lineBreak)
+        li.appendChild(text)
+        list.appendChild(li)
+        list.appendChild(button)
+        button.appendChild(buttonInfo)
+        liInfo.appendChild(textInfo)
+        button.addEventListener("click", function(){
+            // const newList = document.createElement('ul')
+            // const liInfo = document.createElement('li')
+            const textInfo = document.createTextNode(`${person.phone}, ${person.email}, ${person.dob.date}, ${person.dob.age}`)
+            // liInfo.appendChild(textInfo)
+            // newList.appendChild(liInfo)
+            li.appendChild(lineBreak)
+            li.appendChild(textInfo)})
+    }));  
+
+}
 
 //https://randomuser.me/api/?results=5
 // let arrayOfAddress = [];
